0b81f690ff5b21d673fb87f09eebe388e6dce8ac
juliaaaloopez <julia.lopez11@estudiant.upf.edu>
2024-02-26 12:14:23 +0100

    markFileAsFinished

--- P3/main.c
+++ P3/main.c
@@ -20,7 +20,7 @@ void* worker_function(void * arg){
 
         //place semaphore to manage the number of threads accessing the function concurrently
         sem_wait(&semaphore); 
-        int res = getAndReserveFile(&fm, &d); // Reserves a file. The release is missing. Where should you put it?
+        int res = getAndReserveFile(&fm, &d); // Reserves a file 
         sem_post(&semaphore); 
 
         // Case 1
@@ -28,17 +28,27 @@ void* worker_function(void * arg){
             read(d.fdcrc, &crc, sizeof(short int)); // read crc file and put the value in variable crc
             int nBytesReadData = read(d.fddata, buff, 256); // read a block of data file and put it in the buffer
 
-            if (crc != crcSlow(buff, nBytesReadData)) { // compute the crc and compare it to the read crc
-                printf("CRC error in file %s\n", d.filename);
+            if(nBytesReadData == 0){ // there is no more data to read in this file
+                unreserveFile(&fm, &d); 
+                markFileAsFinished(&fm, &d); 
+                
+            } 
+            else{
+                if (crc != crcSlow(buff, nBytesReadData)) { // compute the crc and compare it to the read crc
+                    printf("CRC error in file %s\n", d.filename);
+                }
+                unreserveFile(&fm, &d); 
             }
-            unreserveFile(&fm, &d); 
+            
         }
         
         // Case 2
-        if (res == 1 ){ // no files available and 
-           if(!fm.fileAvailable){
-            pthread_join(thread, NULL);
-           }
+        if (res == 1 && fm.nFilesRemaining > 0){ // no files available and not all files finished
+           pthread_join(*(pthread_t *)arg, NULL); // we make the thread wait for a signal 
+        }
+
+        if (res == 1 && fm.nFilesRemaining == 0){
+            pthread_exit(NULL); //all files are finished
         }
     }
 }
@@ -48,7 +58,8 @@ int main(int argc, char ** argv) {
     pthread_t threadID[fm.nFilesTotal]; // create one thread per file
     for (int i = 0; i < fm.nFilesTotal; ++i) { 
         pthread_t thread;
-        pthread_create(&threadID[i], NULL, worker_function, NULL); // threads execute worker function
+        pthread_create(&threadID[i], NULL, worker_function, &threadID[i]); // threads execute worker function
+        // we pass the threadID to the worker function
     }
 
     for (int i = 0; i < fm.nFilesTotal; ++i) { // wait until all threads have finished
